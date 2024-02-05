d0c15e5215b63759ff48de94393ea3d762f39bca
ABRARgomaa <abrargomaa111@gmail.com>
2024-02-05 23:41:16 +0200

    hi

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -3,3 +3,9 @@ package { 'flask':
   ensure   => '2.1.0',
   provider => 'pip3'
 }
+
+# Ensure the correct version of Werkzeug is installed
+package { 'Werkzeug':
+  ensure   => '0.15.5', # or whichever version is compatible
+  provider => 'pip3'
+}
